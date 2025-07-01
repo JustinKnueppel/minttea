d74c6e447d3c0ca55a3f888c230e33e1e369b8df
Justin Knueppel <justinknueppel@gmail.com>
2025-06-30 22:33:30 -0400

    Update flake to use riot fork

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     riot = {
-      url = "github:riot-ml/riot";
+      url = "github:JustinKnueppel/riot";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.minttea.follows = "/";
     };
@@ -19,67 +19,81 @@
     };
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs =
+    inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }:
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
         let
           inherit (pkgs) ocamlPackages mkShell;
           inherit (ocamlPackages) buildDunePackage;
           version = "0.0.3+dev";
         in
-          {
-            devShells = {
-              default = mkShell {
-                buildInputs = [ ocamlPackages.utop ];
-                inputsFrom = [
-                  self'.packages.spices
-                  self'.packages.default
-                  self'.packages.leaves
-                ];
-              };
-            };
-
-            packages = {
-              default = buildDunePackage {
-                inherit version;
-                pname = "minttea";
-                propagatedBuildInputs = with ocamlPackages; [
-                  inputs'.riot.packages.default
-                  (mdx.override {
-                    inherit logs;
-                  })
-                  inputs'.tty.packages.default
-                  uuseg
-                ];
-                src = ./.;
-              };
-              leaves = buildDunePackage {
-                inherit version;
-                pname = "leaves";
-                propagatedBuildInputs = with ocamlPackages; [
-                  self'.packages.default
-                  (mdx.override {
-                    inherit logs;
-                  })
-                  self'.packages.spices
-                ];
-                src = ./.;
-              };
-              spices = buildDunePackage {
-                inherit version;
-                pname = "spices";
-                propagatedBuildInputs = with ocamlPackages; [
-                  inputs'.colors.packages.default
-                  inputs'.tty.packages.default
-                  (mdx.override {
-                    inherit logs;
-                  })
-                  uuseg
-                ];
-                src = ./.;
-              };
+        {
+          devShells = {
+            default = mkShell {
+              buildInputs = [ ocamlPackages.utop ];
+              inputsFrom = [
+                self'.packages.spices
+                self'.packages.default
+                self'.packages.leaves
+              ];
             };
           };
+
+          packages = {
+            default = buildDunePackage {
+              inherit version;
+              pname = "minttea";
+              propagatedBuildInputs = with ocamlPackages; [
+                inputs'.riot.packages.default
+                (mdx.override {
+                  inherit logs;
+                })
+                inputs'.tty.packages.default
+                uuseg
+              ];
+              src = ./.;
+            };
+            leaves = buildDunePackage {
+              inherit version;
+              pname = "leaves";
+              propagatedBuildInputs = with ocamlPackages; [
+                self'.packages.default
+                (mdx.override {
+                  inherit logs;
+                })
+                self'.packages.spices
+              ];
+              src = ./.;
+            };
+            spices = buildDunePackage {
+              inherit version;
+              pname = "spices";
+              propagatedBuildInputs = with ocamlPackages; [
+                inputs'.colors.packages.default
+                inputs'.tty.packages.default
+                (mdx.override {
+                  inherit logs;
+                })
+                uuseg
+              ];
+              src = ./.;
+            };
+          };
+        };
     };
 }
